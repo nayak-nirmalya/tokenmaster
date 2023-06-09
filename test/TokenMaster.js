95afc23988b3fde60fdd075c51dee22f52293dc7
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-09 09:33:00 +0530

    refractor test code

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -2,18 +2,21 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("TokenMaster", () => {
+  let tokenMaster;
+
+  beforeEach(async () => {
+    const TokenMaster = await ethers.getContractFactory("TokenMaster");
+    tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
+  });
+
   describe("Deployment", () => {
     it("sets the contract name", async () => {
-      const TokenMaster = await ethers.getContractFactory("TokenMaster");
-      let tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
-      let name = await tokenMaster.name();
+      const name = await tokenMaster.name();
       expect(name).to.equal("TokenMaster");
     });
 
     it("sets the contract symbol", async () => {
-      const TokenMaster = await ethers.getContractFactory("TokenMaster");
-      let tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
-      let symbol = await tokenMaster.symbol();
+      const symbol = await tokenMaster.symbol();
       expect(symbol).to.equal("TM");
     });
   });
