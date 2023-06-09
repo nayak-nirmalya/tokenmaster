1593a5b0b4f24d296fcf40d3e93b262a46a9422b
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-09 09:23:12 +0530

    added first smoke test

--- test/TokenMaster.js
+++ test/TokenMaster.js
@@ -1,5 +1,13 @@
-const { expect } = require("chai")
+import { expect } from "chai";
+import { ethers } from "hardhat";
 
 describe("TokenMaster", () => {
-
-})
+  describe("Deployment", () => {
+    it("sets the contract name", async () => {
+      const TokenMaster = await ethers.getContractFactory("TokenMaster");
+      let tokenMaster = await TokenMaster.deploy("TokenMaster", "TM");
+      let name = await tokenMaster.name();
+      expect(name).to.equal("TokenMaster");
+    });
+  });
+});
