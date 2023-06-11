dd259190d5ad7545455152ba749d5dad823462ae
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-11 11:44:46 +0530

    added owner

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -4,8 +4,12 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract TokenMaster is ERC721 {
+    address public owner;
+
     constructor(
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) {}
+    ) ERC721(_name, _symbol) {
+        owner = msg.sender;
+    }
 }
