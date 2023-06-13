8744ec7e5d0983390cf4ebe91bcdabbb63394c62
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-13 22:27:06 +0530

    added func to getOccasion

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -48,4 +48,8 @@ contract TokenMaster is ERC721 {
             _location
         );
     }
+
+    function getOccasion(uint256 _id) public view returns (Occasion memory) {
+        return occasions[_id];
+    }
 }
