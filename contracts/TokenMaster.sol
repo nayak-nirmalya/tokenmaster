32205c1755743b3ab9f9302c63e298109b57242d
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-13 22:02:14 +0530

    added struct Occasion

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -6,10 +6,30 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract TokenMaster is ERC721 {
     address public owner;
 
+    struct Occasion {
+        uint256 id;
+        string name;
+        uint256 costs;
+        uint256 tickets;
+        uint256 maxTickets;
+        string date;
+        string time;
+        string location;
+    }
+
     constructor(
         string memory _name,
         string memory _symbol
     ) ERC721(_name, _symbol) {
         owner = msg.sender;
     }
+
+    function list(
+        string memory _name,
+        uint256 _cost,
+        uint256 _maxTickets,
+        string memory _date,
+        string memory _time,
+        string memory _location
+    ) public {}
 }
