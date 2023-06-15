95968bea1fbacdb985655649700b1428ecb3f792
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-15 08:54:01 +0530

    get current account

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import { useEffect, useState } from 'react'
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation'
-import Sort from './components/Sort'
-import Card from './components/Card'
-import SeatChart from './components/SeatChart'
+import Navigation from "./components/Navigation";
+import Sort from "./components/Sort";
+import Card from "./components/Card";
+import SeatChart from "./components/SeatChart";
 
 // ABIs
-import TokenMaster from './abis/TokenMaster.json'
+import TokenMaster from "./abis/TokenMaster.json";
 
 // Config
-import config from './config.json'
+import config from "./config.json";
 
 function App() {
+  const loadBlockchainData = async () => {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
       <header>
-
-        <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
+        <h2 className="header__title">
+          <strong>Welcome to Tokenmaster</strong>
+        </h2>
       </header>
-
     </div>
   );
 }
